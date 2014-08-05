9c5c09d65f9ca5f4cd600b3923bcb638a5b6a845
Young_4You <evan_young@sina.cn>
2014-08-05 15:58:01 +0800

    Signed-off-by: Young_4You <evan_young@sina.cn>

--- src/com/example/viewpagerfragmentactivity/FragmentA.java
+++ src/com/example/viewpagerfragmentactivity/FragmentA.java
@@ -1,17 +1,23 @@
 package com.example.viewpagerfragmentactivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class FragmentA extends Fragment {
 	private String tag = getClass().getSimpleName();
 	View v;
 	int pageId;
+	
+
+	
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,6 +25,41 @@ public class FragmentA extends Fragment {
 		Log.d(tag, "onCreateView");
 		View v = inflater.inflate(R.layout.tab1_fragment, container, false);
 		TextView tv = (TextView) v.findViewById(R.id.textView1);
+		//add by young 2014-08-05
+		ImageView IV = (ImageView) v.findViewById(R.id.imageView1);
+ 
+		switch(pageId)
+		{
+		case 0:
+			IV.setImageBitmap(BitmapFactory.decodeResource(getResources(),
+					R.drawable.loading_01).copy(Bitmap.Config.ARGB_8888, true));
+			break;
+		case 1:
+			IV.setImageBitmap(BitmapFactory.decodeResource(getResources(),
+					R.drawable.loading_01).copy(Bitmap.Config.ARGB_8888, true));
+			break;
+		case 2:
+			IV.setImageBitmap(BitmapFactory.decodeResource(getResources(),
+					R.drawable.loading_02).copy(Bitmap.Config.ARGB_8888, true));
+			break;
+		case 3:
+			IV.setImageBitmap(BitmapFactory.decodeResource(getResources(),
+					R.drawable.loading_03).copy(Bitmap.Config.ARGB_8888, true));
+			break;
+		case 4:
+			IV.setImageBitmap(BitmapFactory.decodeResource(getResources(),
+					R.drawable.loading_04).copy(Bitmap.Config.ARGB_8888, true));
+			break;
+		case 5:
+			IV.setImageBitmap(BitmapFactory.decodeResource(getResources(),
+					R.drawable.loading_05).copy(Bitmap.Config.ARGB_8888, true));
+			break;
+		case 6:
+			IV.setImageBitmap(BitmapFactory.decodeResource(getResources(),
+					R.drawable.loading_06).copy(Bitmap.Config.ARGB_8888, true));
+			break;
+		}
+ 
 		tv.setText("pageId: " + pageId);
 
 		return v;
